9356bbe7fc23a610b97a5605e54e9c3d75b30200
wendy03 <wendysu03@gmail.com>
2023-02-13 22:46:24 +0800

    add order page

---
+++ src/components/frontend/OrderDetail.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="col-md-4">
+    <div class="border p-4 mb-4">
+      <div v-for="item in cart.carts" :key="item.id" class="d-flex mb-2">
+        <img
+          :src="item.product.imageUrl"
+          :alt="item.product.title"
+          class="me-2"
+          style="width: 48px; height: 48px; object-fit: cover"
+        />
+        <div class="w-100">
+          <div class="d-flex justify-content-between">
+            <p class="mb-0 fw-bold">{{ item.product.title }}</p>
+            <p class="mb-0">NT$ {{ item.product.price }}</p>
+          </div>
+          <p class="mb-0 fw-bold">x {{ item.qty }}</p>
+        </div>
+      </div>
+      <table class="table mt-4 border-top border-bottom text-muted">
+        <tbody>
+          <tr>
+            <th scope="row" class="border-0 px-0 pt-4 font-weight-normal">
+              小計
+            </th>
+            <td class="text-end border-0 px-0 pt-4">NT${{ cart.total }}</td>
+          </tr>
+          <tr>
+            <th scope="row" class="border-0 px-0 pt-0 pb-4 font-weight-normal">
+              付款方式
+            </th>
+            <td class="text-end border-0 px-0 pt-0 pb-4">ApplePay</td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="d-flex justify-content-between mt-4">
+        <p class="mb-0 h4 fw-bold">總計</p>
+        <p class="mb-0 h4 fw-bold">NT${{ cart.final_total }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'pinia';
+import cartStore from '../../stores/frontend/cartStore';
+
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    ...mapActions(cartStore, ['getCarts']),
+  },
+  computed: {
+    ...mapState(cartStore, ['cart']),
+  },
+  mounted() {
+    this.getCarts();
+  },
+};
+</script>
---
+++ src/views/frontend/OrderView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="mt-5">
+    <VueLoading v-model:active="isLoading"></VueLoading>
+    <h2 class="mb-5">訂單資訊</h2>
+    <div class="row flex-row-reverse justify-content-center pb-5">
+      <OrderDetail></OrderDetail>
+      <OrderForm></OrderForm>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'pinia';
+import OrderForm from '../../components/frontend/OrderForm.vue';
+import OrderDetail from '../../components/frontend/OrderDetail.vue';
+import statusStore from '../../stores/statusStore';
+
+export default {
+  data() {
+    return {};
+  },
+  components: { OrderForm, OrderDetail },
+  computed: {
+    ...mapState(statusStore, ['isLoading']),
+  },
+};
+</script>
