d44412d9350ff02ae09a27c7b0059d82a8efa8a1
wendy03 <wendysu03@gmail.com>
2023-02-13 22:44:28 +0800

    update base url

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/week6_vite/',
   resolve: {
-    base: 'week6_vite/',
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
